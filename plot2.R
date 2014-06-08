34c4dd83cb15cfcdb43cb0efc9c0ddaec1791558
Tom Giordano <tomgiordano83@gmail.com>
2014-06-08 21:00:19 +1000

    Add plot2

---
+++ plot2.R
@@ -0,0 +1,17 @@
+file <- "../data/household_power_consumption.txt"
+data <- read.delim(file, header = TRUE, sep=";", na.strings = "?")
+x <- paste(data$Date, data$Time)
+
+data$DateTime <- strptime(x, format = "%d/%m/%Y %T")
+
+dsub <- subset(data, DateTime >= as.POSIXct('2007-02-01 00:00') & DateTime <=
+                as.POSIXct('2007-02-02 23:59'))
+
+png(filename = "plot2.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white", type = c("cairo", "cairo-png", "Xlib", "quartz"))
+
+plot(dsub$DateTime, dsub$Global_active_power, xlab = NA, ylab = "Global Active Power (kilowatts)", 
+     type = "l")
+
+dev.off()
